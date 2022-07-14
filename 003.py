90952fe08a021976b27c973d86870ec8f1ff21e5
Denis <chekmarevdenchik5@gmail.com>
2022-07-15 01:44:42 +0700

    trying to modify 003.py

--- 003.py
+++ 003.py
@@ -9,12 +9,14 @@ from decimal import Decimal
 
 
 def get_min_fractional_part(list: list) -> float:
-    min = 1
-    max = 0
+    min = Decimal('1')
+    max = Decimal('0')
     for i in list:
-        if min % 1 > i % 1: min = i
-        if max % 1 < i % 1: max = i
-    return max - min
+        float_part = Decimal(str(i%1))
+        if min % 1 > float_part: min = i
+        if max % 1 < float_part: max = i
+    result = max - min
+    return float(result.to_eng_string())
 
 
 list = [1.1, 1.2, 3.1, 5, 10.01]
